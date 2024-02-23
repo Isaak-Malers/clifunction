e4c28f7bf42d22c076c71b7b49e99b24f211e2df
isaak <isaak.malers@gmail.com>
2024-02-22 20:46:39 -0700

    How do module docstrings work?  who knows.

--- CliFunction.py
+++ CliFunction.py
@@ -1,11 +1,6 @@
 import inspect
 import sys
 import re
-"""
-Provides the simplest possible wrapper for exposing python functions to be run on a command line!
-Annotate python functions with the [@cli_function] decorator, and annotate the function fully with the builtin python tools.
-The library will generate a man page for your file, and provide helpful error messages when arguments are not formatted correctly.
-"""
 
 
 class FunctionCliException(Exception):
